67825ac6a3d701816a6eabdb70303f795c40b4c0
Berta Vincze <berta.vincze@gmail.com>
2019-06-17 22:46:59 +0200

    Add ThreadList class for displaying process threads in a pop-up window

---
+++ ThreadsList.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace ProcessNote
+{
+    public partial class ThreadsList : Form
+    {
+        Process process;
+        ProcessThreadCollection threads;
+
+        public ThreadsList(Process process)
+        {
+            InitializeComponent();
+            this.process = process;
+            this.threads = process.Threads;
+        }
+
+        private void ThreadsList_Load(object sender, EventArgs e)
+        {
+            foreach (ProcessThread thread in threads)
+            {
+                listBox1.Items.Add(thread.Id);
+            }
+        }
+    }
+}
