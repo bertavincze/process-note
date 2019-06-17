1103a0e6dd1b33e0bb88cb30acb34f0ea9bc2e39
Berta Vincze <berta.vincze@gmail.com>
2019-06-17 23:10:04 +0200

    Add Warning class for displaying pop-up warning dialog

---
+++ Warning.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ProcessNote
+{
+    public partial class Warning : Form
+    {
+        string Message;
+
+        public Warning(string message)
+        {
+            InitializeComponent();
+            Message = message;
+        }
+
+        private void Warning_Load(object sender, EventArgs e)
+        {
+            label1.Text = Message;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+    }
+}
