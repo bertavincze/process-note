d25831c930704b3c4b612aab72a33c1521cb9f14
Berta Vincze <berta.vincze@gmail.com>
2019-06-17 22:14:23 +0200

    Add main display logic to ProcessNote class

---
+++ ProcessNote.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace ProcessNote
+{
+    partial class ProcessNote
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.processNoteBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.listView = new System.Windows.Forms.ListView();
+            this.PID = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ProcessName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label1 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.cpu = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.memory = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.running = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.start = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.processNoteBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listView
+            // 
+            this.listView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.PID,
+            this.ProcessName});
+            this.listView.FullRowSelect = true;
+            this.listView.Location = new System.Drawing.Point(12, 12);
+            this.listView.Name = "listView";
+            this.listView.Size = new System.Drawing.Size(417, 188);
+            this.listView.TabIndex = 0;
+            this.listView.UseCompatibleStateImageBehavior = false;
+            this.listView.View = System.Windows.Forms.View.Details;
+            this.listView.SelectedIndexChanged += new System.EventHandler(this.ListView_SelectedIndexChanged);
+            this.listView.DoubleClick += new System.EventHandler(this.ListView_SelectedIndexChanged);
+            // 
+            // PID
+            // 
+            this.PID.Text = "Process ID";
+            this.PID.Width = 71;
+            // 
+            // ProcessName
+            // 
+            this.ProcessName.Text = "Process Name";
+            this.ProcessName.Width = 81;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 216);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Process Attributes";
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.cpu,
+            this.memory,
+            this.running,
+            this.start});
+            this.listView1.Location = new System.Drawing.Point(12, 241);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(417, 59);
+            this.listView1.TabIndex = 3;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // cpu
+            // 
+            this.cpu.Text = "CPU Usage";
+            this.cpu.Width = 70;
+            // 
+            // memory
+            // 
+            this.memory.Text = "Memory Usage";
+            this.memory.Width = 85;
+            // 
+            // running
+            // 
+            this.running.Text = "Running Time";
+            this.running.Width = 80;
+            // 
+            // start
+            // 
+            this.start.Text = "Start Time";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 316);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Add comment";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(15, 341);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(414, 59);
+            this.textBox1.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 415);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Save";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // ProcessNote
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(441, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listView);
+            this.Name = "ProcessNote";
+            this.Text = "Process Note";
+            this.Load += new System.EventHandler(this.ProcessNote_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.processNoteBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.BindingSource processNoteBindingSource;
+        private System.Windows.Forms.ListView listView;
+        public System.Windows.Forms.ColumnHeader PID;
+        public System.Windows.Forms.ColumnHeader ProcessName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader cpu;
+        private System.Windows.Forms.ColumnHeader memory;
+        private System.Windows.Forms.ColumnHeader running;
+        private System.Windows.Forms.ColumnHeader start;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ ProcessNote.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Threading;
+
+namespace ProcessNote
+{
+    public partial class ProcessNote : Form
+    {
+        public ProcessNote()
+        {
+            InitializeComponent();
+        }
+
+        private Process[] processes = Process.GetProcesses();
+
+        private void ProcessNote_Load(object sender, EventArgs e)
+        {
+            foreach (Process process in processes)
+            {
+                string[] row = new string[] { process.Id.ToString(), process.ProcessName };
+                listView.Items.Add(new ListViewItem(row));
+            }
+        }
+
+        private void ListView_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listView1.Items.Count > 0)
+            {
+                listView1.Items.RemoveAt(0);
+            }
+
+            if (listView.SelectedItems.Count > 0 && listView.SelectedItems[0] != null)
+            {
+                string id = listView.SelectedItems[0].Text;
+
+                foreach (Process process in processes)
+                {
+                    if (process.Id.ToString() == id)
+                    {
+                        PerformanceCounter cpuCounter = new PerformanceCounter("Process", "% Processor Time", process.ProcessName, true);
+                        PerformanceCounter ramCounter = new PerformanceCounter("Process", "Private Bytes", process.ProcessName, true);
+                        double cpu = Math.Round(cpuCounter.NextValue() / Environment.ProcessorCount, 2);
+                        double ram = Math.Round(ramCounter.NextValue() / 1024 / 1024, 2);
+
+                        DateTime startTime = process.StartTime;
+                        TimeSpan runningTime = DateTime.Now - startTime;
+
+                        string[] row = new string[] { cpu + "%", ram + " MB", runningTime.ToString(), startTime.ToString()};
+                        listView1.Items.Add(new ListViewItem(row));
+                    }
+                }
+            }
+        }
+    }
+}
